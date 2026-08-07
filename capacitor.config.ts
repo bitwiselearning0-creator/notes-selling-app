cf188e2ff2835addd9c76c32e4595f5dda65f1ba
Akhalaq Monis <akhalaq@Akhalaqs-MacBook-Air.local>
2026-08-07 22:20:29 +0530

    Configure live server URL in capacitor.config.ts for auto-updating Android App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bitwiselearning.app',
   appName: 'Bitwise Learning',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://bitwise-learning.onrender.com?platform=app',
+    cleartext: true
+  }
 };
 
 export default config;
